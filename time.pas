500887935beee4c9afeb3d8922ab658ba3d8641e
Alex Shevchenko <fff.shev1705@gmail.com>
2023-09-30 11:21:00 +0300

    время

---
+++ time.pas
@@ -0,0 +1,7 @@
+﻿begin
+  var year := ReadInteger('Введите год');
+  if (year mod 4 = 0) and ((year mod 100 <> 0) or (year mod 400 = 0)) then
+    Print('Високосный')
+  else
+    Print('Не високосный');
+end.
